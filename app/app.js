c90194dee72671acb4d6d01b4b8bae26c8e5ff85
BryceLanglotzCS <BryceLanglotzCS@gmail.com>
2015-08-23 14:56:00 -0700

    Join Waitlist Call
    Sign Up button now adds the entered e-mail to the waitlist.

--- app/app.js
+++ app/app.js
@@ -1,4 +1,5 @@
 'use strict';
+Parse.initialize("aS5JfOEzT7lLooye3NQCzkFQagbUXaQVhKX24wnE", "IJxIYp2hyjYPylufvFOiwEGOjkkjqVqg7zz1khAc");
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
@@ -10,3 +11,17 @@ angular.module('myApp', [
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
+
+function joinWaitlist(emailAddress) {
+
+  Parse.Cloud.run('JoinWaitlist', {email:emailAddress}, {
+    success: function(result) {
+      alert("Success")
+      // TODO Handle success
+    },
+    error: function(e) {
+      alert(e.message)
+      // TODO Handle errors
+    }
+  });
+}
