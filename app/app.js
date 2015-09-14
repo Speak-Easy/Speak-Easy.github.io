a42854d379ee656937adfdefb89b2fd9997e6e9c
Michael Zamani <michael.zamani1@gmail.com>
2015-09-14 15:57:04 -0400

    Updates dependencies and html/js of main page.

--- app/app.js
+++ app/app.js
@@ -1,12 +1,17 @@
-'use strict';
+(function() {
+	'use strict';
 
-// Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+	// Declare app level module which depends on views, and components
+	angular.module('app', [
+	  'ui.bootstrap',
+	  'app.jumbotron',
+	  'app.about',
+	  'app.consumer',
+	  'app.restaurant'
+	])
+
+	.controller('AppCtrl', ['$scope', function($scope) {
+		
+	}]);
+
+}());
