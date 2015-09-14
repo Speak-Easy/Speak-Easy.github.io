60344ef88d4824428acf57fdc30561c7e9539e7e
Michael Zamani <michael.zamani1@gmail.com>
2015-09-14 15:57:52 -0400

    Removes stock view code. Refactors ParseService

--- app/services/ParseService.js
+++ app/services/ParseService.js
@@ -1,27 +1,20 @@
-'use strict';
+(function() {
+	'use strict';
 
-var parseServiceModule = angular.module('parseServiceModule', [])
+	var parseServiceModule = angular.module('parseServiceModule', [])
 
-.service('parseService', [function() {
-    var _parse = Parse.initialize('aS5JfOEzT7lLooye3NQCzkFQagbUXaQVhKX24wnE',
-	'IJxIYp2hyjYPylufvFOiwEGOjkkjqVqg7zz1khAc');
+	.service('parseService', [function(parse) {
+	    Parse.initialize('aS5JfOEzT7lLooye3NQCzkFQagbUXaQVhKX24wnE',
+		'IJxIYp2hyjYPylufvFOiwEGOjkkjqVqg7zz1khAc');
 
-    this.joinWaitlist = function(emailAddress) {
-	_parse.Cloud.run(
-	    'JoinWaitlist', 
-	    {email: emailAddress},
-	    {
-		succes: function(result) {
-		    // TODO: Alert user success
-		    console.log(result);
-		},
-		error: function(error) {
-		    // TODO: Alert user of failure
-		    console.log(result);
-		}
+	    this.joinWaitlist = function(emailAddress) {
+			return Parse.Cloud.run('JoinWaitlist', {email: emailAddress}, {});
 	    }
-    });
 
+	    this.isValidEmail = function(emailAddress) {
 
-}]);
+	    }
 
+	}]);
+
+}());
